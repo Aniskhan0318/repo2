869f8e572b998a11dfe829b013164cc2fef080aa
aniskhan <aniskhan16@gmail.com>
2022-09-13 18:25:49 +0500

    ok3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and SVEEEE to reload.
+          Edit <code>src/App.js</code> and SVEwwwwEEE to reload.
         </p>
         <a
           className="App-link"
